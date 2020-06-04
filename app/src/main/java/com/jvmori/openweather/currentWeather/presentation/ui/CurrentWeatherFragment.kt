0f3d6201d051b59a453262c77483464b528e6874
jvMori <iwona.moryc92@gmail.com>
2020-06-04 11:45:26 +0200

    [implemented] created currentWeatherFragment and data binding for layout

---
+++ app/src/main/java/com/jvmori/openweather/currentWeather/presentation/ui/CurrentWeatherFragment.kt
@@ -0,0 +1,11 @@
+package com.jvmori.openweather.currentWeather.presentation.ui
+
+import com.jvmori.openweather.R
+import com.jvmori.openweather.common.presentation.ui.BindingFragment
+import com.jvmori.openweather.databinding.FragmentCurrentWeatherBinding
+
+
+class CurrentWeatherFragment : BindingFragment(R.layout.fragment_current_weather) {
+
+    private val currentWeatherBinding = (binding as FragmentCurrentWeatherBinding)
+}
