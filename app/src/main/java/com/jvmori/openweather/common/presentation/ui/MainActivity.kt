620d6237a75081bbdfd32c2fc2f55957db807090
jvMori <iwona.moryc92@gmail.com>
2020-06-05 19:24:04 +0200

    [refactoring] changed colors, font and improved displaying texts in appbar

--- app/src/main/java/com/jvmori/openweather/common/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/jvmori/openweather/common/presentation/ui/MainActivity.kt
@@ -2,16 +2,34 @@ package com.jvmori.openweather.common.presentation.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.jvmori.openweather.R
 import com.jvmori.openweather.currentWeather.presentation.viewmodels.CurrentWeatherViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
-    private val weatherViewModel : CurrentWeatherViewModel by viewModel<CurrentWeatherViewModel>()
+    private val weatherViewModel: CurrentWeatherViewModel by viewModel<CurrentWeatherViewModel>()
+
+    private val navController by lazy { findNavController(this, R.id.navHostFragment) }
+    private val appBarConfiguration by lazy {
+        AppBarConfiguration(
+            topLevelDestinationIds = setOf(
+                R.id.currentWeatherFragment
+            )
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp() || navController.navigateUp()
+    }
+
 }
